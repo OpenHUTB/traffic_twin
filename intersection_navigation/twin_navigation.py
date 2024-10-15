6350256f850f0f8798618308b0d5e8affafa763b
jiandaoshou-aidehua <2893666867@qq.com>
2024-10-15 15:10:39 +0800

    spawn by time

--- intersection_navigation/twin_navigation.py
+++ intersection_navigation/twin_navigation.py
@@ -1,15 +1,18 @@
 """
-    版本1.0：
-        不考虑车辆生成先后顺序，先默认车辆是在同一时刻生成，再各自设置
-    目标车辆的终点位置。
+    版本3.0：
+        1.车辆按照第一个航点，也就是起始路口的时间来生成，使得车辆生成有先后次序
+        2.增加了车辆生成因碰撞失败的策略
+        3.增加只生成小汽车并随机设置车身颜色
 """
 import carla
 import time
 import csv
+import random
 import math
 import mysql.connector
 import sqlite3
-RES_DRIVING = 20.0  # 判断车辆还有下一个路口航点
+from datetime import datetime
+OFFSET_DISTANCE = 3          # 生成车辆失败时，将 location 后移3m
 WAYPOINTS_FILENAME = 'Waypoints.txt'
 
 from agents.navigation.behavior_agent import BehaviorAgent
@@ -89,6 +92,58 @@ def clear_source(need_to_destroy, waypoints_by_vehicle, intersection_time, vehic
         vehicle_plate_list.remove(vehicle_id)
 
 
+# current_time 是str类型：20240603101544
+def get_timestamp(current_time):
+    # 将字符串转换为 datetime 对象
+    target_date = datetime.strptime(current_time, "%Y%m%d%H%M%S")
+    # 获取从1970年1月1日到这个时间的时间戳,float类型
+    timestamp = target_date.timestamp()
+    return int(timestamp)
+
+
+def generate_random_vehicle_color():
+    # 随机生成颜色值
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    random_color = f'{r}, {g}, {b}'
+    return random_color
+
+
+def spawn_vehicle(vehicle_id, waypoints_by_vehicle, vehicle_waypoint_offset, cursor, filtered_vehicle_blueprints,
+                  world, vehicle_list, vehicle_plate_list, agent_list):
+    first_waypoint = waypoints_by_vehicle[vehicle_id][0]
+    location, ro_yaw = query_location(first_waypoint, cursor)
+    # 生成第一辆车
+    transform = carla.Transform(location, carla.Rotation(yaw=ro_yaw))
+    random_color = generate_random_vehicle_color()
+    vehicle_bp = random.choice(filtered_vehicle_blueprints)
+    vehicle_bp.set_attribute('color', random_color)
+    # 车辆生成失败时返回None
+    vehicle = world.try_spawn_actor(vehicle_bp, transform)
+    # 生成车辆位置被占，报碰撞错误，往后面移动3m重新生成车辆，直到生成成功
+    while vehicle is None:
+        transform.location -= transform.get_forward_vector() * OFFSET_DISTANCE
+        vehicle = world.try_spawn_actor(vehicle_bp, transform)
+
+    # 刷新生成车辆
+    world.tick()
+    # 添加车辆
+    vehicle_list[vehicle_id] = vehicle
+    vehicle_plate_list.append(vehicle_id)
+
+    # 创建代理
+    agent = BehaviorAgent(vehicle, behavior='normal')
+    agent_list[vehicle_id] = agent
+
+    second_waypoint = waypoints_by_vehicle[vehicle_id][1]
+    location, ro_yam = query_location(second_waypoint, cursor)
+    # 先行驶到第二个航点
+    agent.set_destination(location)
+    vehicle_waypoint_offset[vehicle_id] = 2  # 表示车辆在第二个路口位置
+
+
 def main():
     # 连接到carla服务器
     client = carla.Client('localhost', 2000)
@@ -98,6 +153,12 @@ def main():
 
     # 获取车辆蓝图库
     blueprint_library = world.get_blueprint_library()
+    vehicle_blueprints = blueprint_library.filter('vehicle.*')
+    # 过滤掉自行车
+    filtered_vehicle_blueprints = [bp for bp in vehicle_blueprints if 'bike' not in bp.id and
+                                   'omafiets' not in bp.id and
+                                   'century' not in bp.id and
+                                   'vespa' not in bp.id]
 
     # 保存全部车辆
     vehicle_list = {}
@@ -123,15 +184,16 @@ def main():
         waypoints_by_vehicle = {}
         intersection_time = {}
 
-        # 读取车辆数据
+        # 后续还得嵌入while true循环, 边读边跑程序
+        # 读取车辆数据,从文本读数据默认是字符串类型
         with open(waypoints_file, 'r', encoding='utf-8') as f:
             reader = csv.reader(f, delimiter=';')
             for row in reader:
                 vehicle_id = int(row[0])   # int
-                t = row[2]                 # float
+                t = row[2]                 # str
                 inter = row[4]             # str
                 lane = int(row[5]) + 1     # int
-                direct = row[6]            # char
+                direct = row[6]            # str
 
                 # 将路口、车道和方向存储在 waypoints_by_vehicle 中，编号相同的数据存为列表
                 if vehicle_id in waypoints_by_vehicle:
@@ -145,9 +207,17 @@ def main():
                 else:
                     intersection_time[vehicle_id] = [t]
 
-        # 按车辆起点位置的时间排序
+        # 按车辆起点位置的时间排序,另外python3.6以后字典是有序的
         sorted_intersection_time = sort_spawn_time(intersection_time)
-        # start_time = time.time()  # 记录当前真实时间
+
+        # 将 intersection_time 拆分为两部分 vehicle_id 和 起始时间start_time
+        vehicle_id_list = []
+        start_time = []
+
+        # 遍历字典，提取车辆ID和经过第一个路口的时间
+        for vehicle_id, times in sorted_intersection_time.items():
+            vehicle_id_list.append(vehicle_id)  # 添加车辆id到列表
+            start_time.append(times[0])  # 提取并添加第一个路口的时间
 
         # 连接到 MySQL 数据库
         conn = mysql.connector.connect(
@@ -161,54 +231,64 @@ def main():
         # 创建一个游标对象
         cursor = conn.cursor()
 
-        # 先生成全部车辆
-        # 获取第一个路点生成车辆
-        for vehicle_id, waypoints in waypoints_by_vehicle.items():
-            first_waypoint = waypoints[0]
-            location, ro_yaw = query_location(first_waypoint, cursor)
+        # 默认运行代码则开始最早的一辆车或者一批车辆的孪生
+        # 先生成最早的一辆车或者一批车辆，并设置路口终点
+        # 从起始时间考虑，判断第一批有几辆车
+        earliest_vehicle_num = start_time.count(start_time[0])
+        # 根据 earliest_vehicle_num 来决定循环的次数
+        count = 0
+        for vehicle_id in vehicle_id_list:
+            spawn_vehicle(vehicle_id, waypoints_by_vehicle, vehicle_waypoint_offset, cursor, filtered_vehicle_blueprints,
+                          world, vehicle_list, vehicle_plate_list, agent_list)
+            count += 1
+            # 最早一批次车辆生成完毕, 结束循环
+            if earliest_vehicle_num == count:
+                break
 
-            # print(f"{type(ro_yaw)}")
-            # print(ro_yaw)
-            # 生成车辆
-            transform = carla.Transform(location, carla.Rotation(yaw=ro_yaw))
-            vehicle_bp = blueprint_library.find('vehicle.audi.tt')
-            vehicle_bp.set_attribute('color', '100, 250, 250')
-            vehicle = world.spawn_actor(vehicle_bp, transform)
-
-            # 刷新生成车辆
-            world.tick()
-            # 添加车辆
-            vehicle_list[vehicle_id] = vehicle
-            vehicle_plate_list.append(vehicle_id)
-
-            # 创建代理
-            agent = BehaviorAgent(vehicle, behavior='normal')
-            agent_list[vehicle_id] = agent
-
-            second_waypoint = waypoints[1]
-            location, ro_yam = query_location(second_waypoint, cursor)
-            # 先行驶到第二个航点
-            agent.set_destination(location)
-            vehicle_waypoint_offset[vehicle_id] = 2  # 表示车辆在第一个位置
-
+        # 记录最早批次生成时间戳,2024年678月
+        first_stamp = get_timestamp(start_time[0])
+        # 得到当前系统的时间戳，只考虑整数部分(2024年10月)
+        first_system_stamp = int(time.time())
+        # 记录下次该生成车辆在 vehicle_id_list 中的下标
+        next_vehicle_num = earliest_vehicle_num
+        # print(next_vehicle_num)
+        # print(start_time[next_vehicle_num])
         while True:
-
-            # # 模拟和真实时间一致
-            # current_time = time.time()
-            # elapsed_time = current_time - start_time  # 计算经过的真实时间
-            # if elapsed_time >= 1:  # 每经过1秒推进仿真
-            #     world.tick()
-            #     start_time = time.time()  # 重置计时器
+            num = 0
             time.sleep(0.05)
             world.tick()
 
+            # 接下来陆续按时间的先后顺序生成车辆
+            # 下一批生成车辆的时间戳 - 上一批生成车辆的时间戳
+            if next_vehicle_num < len(start_time):
+                this_stamp = get_timestamp(start_time[next_vehicle_num])
+                during_to_next_vehicle = this_stamp - first_stamp
+                # 当前系统时间戳 - 上次生成车辆的系统时间戳
+                this_system_stamp = int(time.time())
+                during_system_time = this_system_stamp - first_system_stamp
+
+                # 已经过了相同的时间, 该生成下一批次车辆
+                if during_system_time == during_to_next_vehicle:
+                    num = start_time.count(start_time[next_vehicle_num])
+                    index = 0
+                    # 生成车辆
+                    for vehicle_id in vehicle_id_list[next_vehicle_num:]:
+                        spawn_vehicle(vehicle_id, waypoints_by_vehicle, vehicle_waypoint_offset, cursor,
+                                      filtered_vehicle_blueprints, world, vehicle_list, vehicle_plate_list, agent_list)
+                        index += 1
+                        if index == num:
+                            break
+                    next_vehicle_num += num
+                    first_stamp = this_stamp
+                    first_system_stamp = this_system_stamp
+
+            # 更换车辆的终点坐标
             for vehicle_id, agent in agent_list.items():
 
                 # 注意：先判断车辆到达了最后一个航点，选择将车辆销毁
-
                 if agent.done() and vehicle_waypoint_offset[vehicle_id] >= len(waypoints_by_vehicle[vehicle_id]):
 
-                    # ，还得加个判断车辆是否已经销毁，否则，会重复销毁，报错
+                    # 还得加个判断车辆是否已经销毁，否则，会重复销毁，报错
                     # RuntimeError: trying to operate on a destroyed actor; an actor's function was called,
                     # but the actor is already destroyed.
                     if vehicle_id not in destroyed_vehicle_id:
@@ -239,9 +319,11 @@ def main():
                 # 清空已销毁车辆所占资源
                 clear_source(need_to_destroy, waypoints_by_vehicle, intersection_time, vehicle_list,
                              vehicle_waypoint_offset, vehicle_plate_list, agent_list)
+
             # 清空 need_to_destroy
-            #
+            need_to_destroy.clear()
             is_to_destroy = False
+
     finally:
         world.apply_settings(origin_settings)
 
