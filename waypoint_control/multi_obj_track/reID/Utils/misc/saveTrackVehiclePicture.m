2fd90990edaecf4160225266fe9ab65d76979032
liuao08 <166922672+liuao08@users.noreply.github.com>
2025-12-26 18:09:57 +0800

    轨迹标签漏洞修复 (#94)
    * 调整数据格式
    
    * 自定义数据集模型训练
    
    * 在deepsort方法中得到单路口轨迹
    
    * 调整自定义数据集配置文件
    
    * 添加.yaml文件
    
    * 优化调用逻辑
    
    * 将3D检测框输入到JPDA跟踪器
    
    * 关闭测试集数据增强，优化数据路径并生成人车轨迹
    
    * 为轨迹添加类别标签
    
    * 漏洞修复

--- waypoint_control/multi_obj_track/reID/Utils/misc/saveTrackVehiclePicture.m
+++ waypoint_control/multi_obj_track/reID/Utils/misc/saveTrackVehiclePicture.m
@@ -19,7 +19,9 @@ function saveTrackVehiclePicture(trkIDimg2DBox, savedImg, junc)
     for k = 1:numel(trkIDimg2DBox) % 遍历 trkIDimg2DBox 中的每个元素
         trkID = trkIDimg2DBox{k}.trkID; % 获取当前元素的轨迹ID
         closestBox = trkIDimg2DBox{k}.Box; % 获取当前元素的检测框（Box）
+        category = trkIDimg2DBox{k}.Category; % 获取当前元素的类别
         outputImgPath = fullfile(dataPath, sprintf('%d.jpeg', trkID));  % 使用图片索引命名
+        outputCategoryPath = fullfile(dataPath, sprintf('%d.mat', trkID));  % 使用图片索引命名
         % 检查该文件是否已经存在
         if exist(outputImgPath, 'file')
             continue;  % 如果文件存在，跳过当前迭代
@@ -29,8 +31,10 @@ function saveTrackVehiclePicture(trkIDimg2DBox, savedImg, junc)
         % 缩放到指定大小 224x224
         resizedImg = imresize(croppedImg, [224, 224]);
         % 保存裁剪后的图像
-        
         imwrite(resizedImg, outputImgPath);
+        % 保存轨迹标签信息
+        trajcategory = struct('trkID', trkID, 'Category', category);
+        save(outputCategoryPath, 'trajcategory', '-v7.3');
     end
 
 
