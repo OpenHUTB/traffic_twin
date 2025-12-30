f9b6a800d5048e232935cd3d06f4abb9db9cbe4b
liuao08 <2496556459@qq.com>
2025-12-30 17:36:09 +0800

    得到孪生轨迹

---
+++ waypoint_control/multi_obj_track/mergetraj.m
@@ -0,0 +1,9 @@
+% 加载两个 .mat 文件
+data1 = load('Town10HD_Opt/pedestrian_ground_truth.mat');
+data2 = load('Town10HD_Opt/vehicle_ground_truth.mat');
+
+% 横向合并（水平连接）
+truth_data = [data1.pedestrian_cells, data2.vehicle_cells];
+
+% 保存合并后的数据到新文件
+save('ground_truth.mat', 'truth_data');
