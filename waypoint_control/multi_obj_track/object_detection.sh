f505f5bacde2c5cf7e2aa81044d9e78a02a0c46b
liuao08 <166922672+liuao08@users.noreply.github.com>
2026-07-16 22:18:08 +0800

    论文results部分撰写 (#164)

--- waypoint_control/multi_obj_track/object_detection.sh
+++ waypoint_control/multi_obj_track/object_detection.sh
@@ -11,6 +11,6 @@ cd $WORK_DIR
 
 # 运行 Python 脚本
 python -m pcdet.datasets.custom.custom_dataset create_custom_infos tools/cfgs/dataset_configs/custom_dataset.yaml
-python tools/test.py --cfg_file output/cfgs/custom_models/pv_rcnn/default/pv_rcnn.yaml  --ckpt latest_model.pth
+python tools/test.py --cfg_file output/cfgs/custom_models/pv_rcnn/default/pv_rcnn/default/pv_rcnn.yaml  --ckpt output/cfgs/custom_models/pv_rcnn/default/pv_rcnn/default/ckpt/latest_model.pth
 
 echo "========== 完成自动化目标检测 =========="
